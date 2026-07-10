ddb9037a02f7d12f5165a43be818b3a7a0de7010
Aryanhapani <hapaniaryan51@gmail.com>
2026-07-10 09:26:00 +0530

    H

--- intermidate 1/checkbinary.c
+++ intermidate 1/checkbinary.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main(){
-   int n=15121;
-   int flag=1;
-   while (n > 0)
+   int n=1011001111;
+   int is_binary=1;
+   while (n != 0)
    {
-     int lg=n%10;
-     if(lg != 1 && lg !=0){
-        flag=0;
+     int ld=n%10;
+     if(ld < 0 || ld > 1){
+        is_binary=0;
         break;
      }
     
      n=n/10;
    }
    
-   if(flag){
+   if(is_binary){
     printf("true");
    }else{
     printf("false");
